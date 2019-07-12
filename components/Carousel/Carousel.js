527bf0c411e6ecf76bc1b3f9ec19def9149e6b86
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-12 18:09:54 +0100

    WIP: stretch, added carousel and click event listeners on buttons

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -9,7 +9,7 @@
 
 /* HTML:
   <div class="carousel">
-    <div class="left-button"> < </div>
+    <div class="carousel"> < </div>
     <img src="./assets/carousel/mountains.jpeg" />
     <img src="./assets/carousel/computer.jpeg" />
     <img src="./assets/carousel/trees.jpeg" />
@@ -17,3 +17,49 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function Carousel() {
+
+  const carouselContainer = document.createElement('div')
+  const carouselLeftButton = document.createElement('div')
+  const carouselImageOne = document.createElement('img')
+  const carouselImageTwo = document.createElement('img')
+  const carouselImageThree = document.createElement('img')
+  const carouselImageFour = document.createElement('img')
+  const carouselRightButton = document.createElement('div')
+
+  carouselContainer.classList.add('carousel')
+  carouselLeftButton.classList.add('left-button')
+  carouselRightButton.classList.add('right-button')
+  carouselImageOne.src = './assets/carousel/mountains.jpeg'
+  carouselImageTwo.src = './assets/carousel/computer.jpeg'
+  carouselImageThree.src = './assets/carousel/trees.jpeg'
+  carouselImageFour.src = './assets/carousel/turntable.jpeg'
+
+  carouselContainer.appendChild(carouselLeftButton)
+  carouselContainer.appendChild(carouselImageOne)
+  carouselContainer.appendChild(carouselImageTwo)
+  carouselContainer.appendChild(carouselImageThree)
+  carouselContainer.appendChild(carouselImageFour)
+  carouselContainer.appendChild(carouselRightButton)
+
+  carouselLeftButton.addEventListener('click', clickLeftButton)
+  carouselRightButton.addEventListener('click', clickRightButton)
+
+  return carouselContainer
+}
+
+const newCarousel = Carousel()
+const mainCarouselContainer = document.querySelector('.carousel-container')
+
+mainCarouselContainer.appendChild(newCarousel)
+
+function clickLeftButton() {
+  console.log('click left')
+}
+
+function clickRightButton() {
+  console.log('click right')
+}
+
+
